0d2c89d35d56fe093ab54c58078eb1c6abfcfe3c
ArtemKvashnin <artemykv@gmail.com>
2023-08-02 20:25:22 +0500

    add project to repo

---
+++ VideoGameBuddy/VideoGameBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoGameBuddyApp.swift
+//  VideoGameBuddy
+//
+//  Created by Artem Kvashnin on 02.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct VideoGameBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
